2e8028d0fc472ab0182d976467379fa5c50f7030
Paola Ducolin <paoletta84@hotmail.com>
2020-05-08 12:51:33 +0200

    refactor: use a single state for data

--- src/popup/App.tsx
+++ src/popup/App.tsx
@@ -1,6 +1,6 @@
 import { hot } from 'react-hot-loader';
 import { browser } from 'webextension-polyfill-ts';
-import React, { useState, useCallback } from 'react';
+import React, { useState, useCallback, useEffect } from 'react';
 import {
     Button,
     FilterList,
@@ -20,127 +20,155 @@ import emptyInbox from './assets/empty_inbox.svg';
 import './style.css';
 import { MergeRequestsDetails } from '../background/types';
 import { getHumanReadableDate } from './helpers';
+import { getMergeRequestList, MergeRequestSendMessageReply } from '../libs/mergeRequestDownloader';
+
+type AppStatus = 'idle' | 'loading' | 'success' | 'error';
 
 const App = () => {
-    const fetchingContentState = <Text>Fetching content...</Text>;
-    const [mrList, updateList] = useState(fetchingContentState);
-    const [mrToReview, setMrToReview] = useState('0');
-    const [mrReviewed, setMrReviewed] = useState('0');
-    const [mrRatio, setMrRatio] = useState(100);
-    const [lastUpdateDateUnix, setLastUpdateDateUnix] = useState(0);
+    const [appStatus, setAppStatus] = useState<AppStatus>('idle');
+    const [errorMessage, setErrorMessage] = useState('');
+    const [mrData, setMrData] = useState<MergeRequestSendMessageReply>({
+        error: '',
+        mrAssigned: [],
+        mrToReview: 0,
+        mrGiven: [],
+        mrReviewed: 0,
+        lastUpdateDateUnix: Date.now()
+    });
 
-    const [tabSelected, setTabSelected] = useState('0');
+    const [currentTab, setCurrentTab] = useState(0);
 
     const openSettings = useCallback(() => browser.runtime.openOptionsPage(), []);
 
-    let type = 'getMRs';
-
-    interface SendMsgResponse {
-        mrAssigned: MergeRequestsDetails[];
-        mrToReview: number;
-        mrGiven: MergeRequestsDetails[];
-        mrReviewed: number;
-        lastUpdateDateUnix: number;
-    }
-    interface SendMsgError {
-        error: string;
-    }
-    const sendMsg = useCallback((event: any) => {
-        if (event && event.target.dataset.key) {
-            setTabSelected(event.target.dataset.id);
-            type = event.target.dataset.key;
-        }
-        browser.runtime
-            .sendMessage({ type: 'getMRs' })
-            .then((response: SendMsgResponse | SendMsgError) => {
+    // useEffect vs useCallback
+    // https://medium.com/@infinitypaul/reactjs-useeffect-usecallback-simplified-91e69fb0e7a3
+    const fetchData = useEffect(() => {
+        setAppStatus('loading');
+        getMergeRequestList()
+            .then((response) => {
                 if (!response) {
-                    // This could infinite loop, so let's see for a correct retry
-                    console.log('Retrying');
-                    return setTimeout(sendMsg(type), 1000);
+                    setAppStatus('error');
+                    setErrorMessage('Something went wrong');
+                    return;
                 }
 
-                if ('error' in response) {
-                    return updateList(
-                        <Flash m={2} scheme="red">
-                            {response.error}
-                        </Flash>
-                    );
+                setMrData(response);
+
+                if (response.error !== '') {
+                    setAppStatus('error');
+                    setErrorMessage(response.error);
+                    return;
                 }
 
-                let mrNewList: MergeRequestsDetails[];
-                if (type === 'getMRs') {
-                    mrNewList = response.mrAssigned;
-                } else {
-                    mrNewList = response.mrGiven;
-                }
-                console.log('Displayed list', mrNewList);
-
-                if (mrNewList.length === 0) {
-                    updateList(<img src={emptyInbox} className={'emptyInbox'} />);
-                } else {
-                    const listItems = mrNewList.map((mr) => {
-                        return <MergeRequest mr={mr} key={mr.id} />;
-                    });
-
-                    updateList(<FilterList className={'mrList'}>{listItems}</FilterList>);
-                }
-
-                setMrToReview(`${response.mrToReview}`);
-                setMrReviewed(`${response.mrReviewed} / ${response.mrGiven.length}`);
-                setMrRatio(Math.floor(((mrNewList.length - response.mrToReview) / mrNewList.length) * 100) || 100);
-                setLastUpdateDateUnix(response.lastUpdateDateUnix);
+                setAppStatus('success');
             })
-            .catch((error) => console.error(error));
+            .catch((error) => {
+                setAppStatus('error');
+                setErrorMessage('Something went wrong');
+                console.error(error);
+            });
     }, []);
 
-    if (mrList === fetchingContentState) {
-        sendMsg(null);
-    }
+    const getContent = () => {
+        if (appStatus === 'idle') {
+            return <Text>Fetching content...</Text>;
+        }
+
+        if (appStatus === 'loading') {
+            return <Text>Fetching content...</Text>;
+        }
+
+        if (appStatus === 'error') {
+            <Flash m={2} scheme="red">
+                {errorMessage}
+            </Flash>;
+        }
+
+        // show data
+        let mrList = [];
+        if (currentTab === 0) {
+            mrList = mrData.mrAssigned;
+        } else {
+            mrList = mrData.mrGiven;
+        }
+
+        console.log('Displayed list', mrList);
+
+        if (mrList.length === 0) {
+            return <img src={emptyInbox} className={'emptyInbox'} />;
+        }
+        return (
+            <FilterList className={'mrList'}>
+                {mrList.map((mr: MergeRequestsDetails) => (
+                    <MergeRequest mr={mr} key={mr.id} />
+                ))}
+            </FilterList>
+        );
+    };
+
+    const getMrRatio = () => {
+        if (appStatus !== 'success' || mrData === null) {
+            return 100;
+        }
+        let mrList = [];
+        if (currentTab === 0) {
+            mrList = mrData.mrAssigned;
+        } else {
+            mrList = mrData.mrGiven;
+        }
+        return Math.floor(((mrList.length - mrData.mrToReview) / mrList.length) * 100);
+    };
 
     return (
         <ThemeProvider theme={primer}>
             <div className={'container'}>
                 <TabNav aria-label="Main" mb={2}>
                     <TabNav.Link
-                        onClick={sendMsg}
+                        onClick={() => setCurrentTab(0)}
                         data-key={'getMRs'}
                         data-id={'0'}
                         href="#MR"
-                        className={tabSelected === '0' ? 'selected' : ''}
+                        className={currentTab === 0 ? 'selected' : ''}
                     >
                         To Review{' '}
                         <Label variant="small" bg="#dc3545">
-                            {mrToReview}
+                            {mrData.mrToReview}
                         </Label>
                     </TabNav.Link>
                     <TabNav.Link
-                        onClick={sendMsg}
+                        onClick={() => setCurrentTab(1)}
                         data-key={'getMyMRs'}
                         data-id={'1'}
                         href="#MyMR"
-                        className={tabSelected === '1' ? 'selected' : ''}
+                        className={currentTab === 1 ? 'selected' : ''}
                     >
                         Under Review{' '}
-                        <Tooltip aria-label={mrReviewed + ' have been reviewed'} direction="s">
+                        <Tooltip aria-label={mrData.mrReviewed + ' have been reviewed'} direction="s">
                             <Label variant="small" bg="#28a745">
-                                {mrReviewed}
+                                {mrData.mrReviewed}
                             </Label>
                         </Tooltip>
                     </TabNav.Link>
                 </TabNav>
-                {mrList}
+                {getContent()}
                 <Flex flexWrap="nowrap">
                     <Tooltip
                         className={'progressBar'}
-                        aria-label={`${mrRatio}% done, keep the good reviews!`}
+                        aria-label={`${getMrRatio()}% done, keep the good reviews!`}
                         direction="n"
                     >
-                        <ProgressBar progress={mrRatio} />
+                        <ProgressBar progress={getMrRatio()} />
                     </Tooltip>
 
                     <div style={{ marginTop: '6px' }}>
-                        <Tooltip aria-label={'Last update: ' + getHumanReadableDate(lastUpdateDateUnix)} direction="n">
-                            <Button onClick={sendMsg} variant={'small'} mr={2}>
+                        <Tooltip
+                            aria-label={
+                                'Last update: ' +
+                                getHumanReadableDate(mrData !== null ? mrData.lastUpdateDateUnix : Date.now())
+                            }
+                            direction="n"
+                        >
+                            <Button onClick={() => fetchData} variant={'small'} mr={2}>
                                 <Octicon icon={Sync} /> Refresh
                             </Button>
                         </Tooltip>
