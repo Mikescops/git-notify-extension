840110b015d36ea8e1ec7997be96ba19780a40ec
pducolin <45568537+pducolin@users.noreply.github.com>
2020-05-09 19:15:38 +0200

    Refactor/use single state for data (#5)
    * refactor: move fetch data function to lib
    
    * refactor: use a single state for data
    
    * fix: handle undefined list
    
    * fix: handle undefined error, cleanup logs
    
    * refactor(Avatar): rename Avatars to AvatarWithTooltip
    
    * refactor(MergeRequest): use slice instead of splice on avatars, add + for > 3 reviewers
    
    * refactor(App): remove data-key, data-id and href
    
    * fix(App): add missing dependency in getContent, add eslint react hook rules
    
    * refactor(options): add check icon when option is set in local storage
    
    * fix: fix eslint warnings
    
    * refactor(libs): use callbacks in etMergeRequestList
    
    * fix(Popup): set data after all checks, bring back review ratio
    
    * fix(Popup): fixes from review
    
    * style(MergeRequest): increase max width size for avatars, reduce branch max-size

---
+++ src/libs/mergeRequestDownloader.ts
@@ -0,0 +1,41 @@
+import { browser } from 'webextension-polyfill-ts';
+import { MergeRequestsDetails } from '../background/types';
+
+const REQUEST_TYPE_GET_MERGE_REQUESTS = 'getMRs';
+
+export interface MergeRequestSendMessageReply {
+    mrAssigned: MergeRequestsDetails[];
+    mrToReview: number;
+    mrGiven: MergeRequestsDetails[];
+    mrReviewed: number;
+    lastUpdateDateUnix: number;
+    error?: string;
+}
+
+export const getMergeRequestList = (): Promise<MergeRequestSendMessageReply> => {
+    return browser.runtime
+        .sendMessage({ type: REQUEST_TYPE_GET_MERGE_REQUESTS })
+        .then((response) => {
+            if (!response) {
+                return {
+                    error: 'No reponse received',
+                    mrAssigned: [],
+                    mrToReview: 0,
+                    mrGiven: [],
+                    mrReviewed: 0,
+                    lastUpdateDateUnix: Date.now()
+                };
+            }
+            return response;
+        })
+        .catch((error) => {
+            return {
+                error: error,
+                mrAssigned: [],
+                mrToReview: 0,
+                mrGiven: [],
+                mrReviewed: 0,
+                lastUpdateDateUnix: Date.now()
+            };
+        });
+};
