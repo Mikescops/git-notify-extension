749564708a20196290dae32ddb89e87b0f702a44
Bradley Cushing <bradley.cushing@dashlane.com>
2020-06-18 22:08:13 -0400

    refactor(empty): move empty container into separate component

---
+++ src/popup/components/EmptyItems.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+import emptyInbox from '../assets/empty_inbox.svg';
+import './style.css';
+
+export const EmptyItems = () => (
+    <div className={'emptyContainer'}>
+        <img src={emptyInbox} className={'emptyInbox'} />
+    </div>
+);
