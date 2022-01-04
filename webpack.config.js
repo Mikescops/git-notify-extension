cb35cfd4be885ce3e7eda5aa6931c3b461b4c0ff
Corentin Mors <corentin.mors@dashlane.com>
2022-01-04 09:46:14 +0100

    Build for manifest v3

--- webpack.config.js
+++ webpack.config.js
@@ -1,2 +1,9 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
-module.exports = require('neutrino')().webpack();
+const neutrino = require('neutrino');
+
+const confManifestV2 = require('./.neutrinorc-v2.js');
+
+const configV3 = neutrino().webpack(); // by default neutrino load .neutrinorc.js
+const configV2 = neutrino(confManifestV2).webpack();
+
+module.exports = [configV2, configV3];
