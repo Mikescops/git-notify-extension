8d5bdd5f4c6c88e4880c6fce98c74b9f38e34053
Corentin Mors <corentin@dashlane.com>
2024-01-09 09:55:50 +0100

    Limit requests size

--- src/background/endpoints/getProjectsList.ts
+++ src/background/endpoints/getProjectsList.ts
@@ -5,5 +5,5 @@ import { initGitlabApi } from '../utils/initGitlabApi';
 export const getProjectsList = async (): Promise<GitlabTypes.GroupSchema[]> => {
     const settings = await getSettings();
     const gitlabApi = initGitlabApi(settings);
-    return await gitlabApi.Groups.all({ min_access_level: 30, top_level_only: true });
+    return await gitlabApi.Groups.all({ min_access_level: 30, top_level_only: true, perPage: 100, maxPages: 5 });
 };
